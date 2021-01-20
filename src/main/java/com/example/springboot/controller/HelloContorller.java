669ecd0f25ce01b4d4f91be51e7e265bf2b31b61
kyawlintunklt <kyawlin.tun@ascendcorp.com>
2021-01-20 15:38:25 +0630

    first commit

---
+++ src/main/java/com/example/springboot/controller/HelloContorller.java
@@ -0,0 +1,13 @@
+package com.example.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContorller {
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello world";
+    }
+}
